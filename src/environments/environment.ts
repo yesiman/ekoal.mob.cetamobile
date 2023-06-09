0f48a15bc51b98419c6d12c77121d9ee58aa990d
Fabrice GRENOUILLET <fab.grenouillet@gmail.com>
2023-06-09 14:04:04 +0400

    up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,36 +14,3 @@ export const environment = {
     measurementId: "G-B1MD3Z3QJ4"
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-
-/*
-{
-  dateObs:Date,
-  espece:number,
-  determination:number,
-  sexe:number,
-  animalMort:number,
-  dateEchou:date,
-  dateRemf:date,
-  action:number,
-  circonst:number,
-  nomInform:string,
-  region:number,
-  district:number,
-  commune:number,
-  village:string,
-  lat:string,
-  long:string,
-  comments:string,
-  observ:string,
-  animaux:string,
-  images:[]
-}*/
